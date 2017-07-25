943a3c4c54782e9ef2687a19769a3875ff293994
chaofengwang <1559878532@qq.com>
2017-07-25 15:33:34 +0800

    项目初始化

---
+++ KeyBoardProgress/KeyBoardProgress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyBoardProgress
+//
+//  Created by WCF on 2017/7/25.
+//  Copyright © 2017年 WCF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeyBoardProgress/KeyBoardProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeyBoardProgress
+//
+//  Created by WCF on 2017/7/25.
+//  Copyright © 2017年 WCF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
